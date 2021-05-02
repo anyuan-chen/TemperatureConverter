ad80f44f27fb344fc5769ad14c02b12073548d1b
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-05-02 13:06:17 -0400

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Andrew Chen on 2021-05-02.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
